15412ee07788d6e81a51266995bc22867e584cc0
naixsu <plnacua02@gmail.com>
2023-12-14 00:17:21 +0800

    Place notes

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -9,8 +9,15 @@ class_name Main
 @onready var audio : AudioStreamPlayer2D = $Audio
 
 var path = "D:\\osu!\\Songs"
-var og_play_area : Vector2 = Vector2(512, 384)
+var OGPlayArea : Vector2 = Vector2(512, 384)
 var offset : Vector2 = Vector2(408, 196)
+var songLength : float = 0
+var metadata : Dictionary = {
+	"General" : {},
+	"Metadata": {},
+	"Difficulty": {},
+	"HitObjects": [],
+}
 
 
 
@@ -19,11 +26,13 @@ func _ready():
 	var i = 0
 	var mp3 : AudioStream = beatmaps[i].mp3
 	var beatmap = beatmaps[i].beatmap
+	read_osu_file(beatmap)
+	
+	#print(metadata["Difficulty"])
 	
 	set_audio_stream(mp3)
-	#audio.play()
-	var metadata = read_osu_file(beatmap)
-	#print(metadata)
+	songLength = set_song_length()
+	
 	
 	place_obects(metadata["HitObjects"])
 
@@ -32,17 +41,16 @@ func _ready():
 func set_audio_stream(mp3: AudioStream) -> void:
 	audio.set_stream(mp3)
 
+## Set the song length in s to ms
+func set_song_length() -> int:
+	var length: float = audio.stream.get_length()
+	var ms: int = length * 1000
+	return ms
+
 ## Read a .txt file that was converted to an .osu file and parse it
-func read_osu_file(beatmap: String) -> Dictionary:
+func read_osu_file(beatmap: String) -> void:
 	var file = FileAccess.open(beatmap, FileAccess.READ)
 	var text = file.get_as_text()
-	
-	var metadata : Dictionary = {
-		"General" : {},
-		"HitObjects": [],
-		"Metadata": {}
-	}
-	
 	var current_metadata : String = ""
 	
 	while not file.eof_reached():
@@ -51,6 +59,8 @@ func read_osu_file(beatmap: String) -> Dictionary:
 		if "[General]" in line:
 			current_metadata = line
 		if "[Metadata]" in line:
+			current_metadata = line
+		if "[Difficulty]" in line:
 			current_metadata = line
 		if "[HitObjects]" in line:
 			current_metadata = line
@@ -64,6 +74,10 @@ func read_osu_file(beatmap: String) -> Dictionary:
 				var key_value = line.split(':')
 				if len(key_value) == 2:
 					metadata["Metadata"][key_value[0]] = key_value[1]
+			"[Difficulty]":
+				var key_value = line.split(':')
+				if len(key_value) == 2:
+					metadata["Difficulty"][key_value[0]] = key_value[1]
 			"[HitObjects]":
 				var raw_data = line.split(',')
 				if len(raw_data) >= 4:
@@ -75,23 +89,24 @@ func read_osu_file(beatmap: String) -> Dictionary:
 						}
 					metadata["HitObjects"].append(data)
 	file.close()
-	
-	return metadata
 
 ## Places objects in the play area (scaled)
 func place_obects(hitObjects: Array) -> void:
 	for obj in hitObjects:
 		var x = obj["x"].to_int()
 		var y = obj["y"].to_int()
-		var time = obj["time"]
-		var type = obj["type"]
+		var time = obj["time"].to_int()
+		var type = obj["type"].to_int()
 		var scaled_xy = get_scale_coords(x, y)
 		
 		var note = Note.instantiate()
 		noteGroup.add_child(note)
 		note.global_position = scaled_xy
 		
-		break
+		var circleSize = float(metadata["Difficulty"]["CircleSize"])
+		note.scale = Vector2(circleSize, circleSize)
+		
+		
 		
 		
 ## Get scaled coordinates given an (x, y)
