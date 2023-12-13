17d147853424e51f333405584cb2d31a828646cd
naixsu <plnacua02@gmail.com>
2023-12-13 23:04:01 +0800

    Parse osu file

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -16,7 +16,8 @@ func _ready():
 	
 	set_audio_stream(mp3)
 	#audio.play()
-	read_osu_file(beatmap)
+	var metadata = read_osu_file(beatmap)
+	print(metadata)
 
 
 ## Set the AudioStreamPlayer's stream
@@ -24,9 +25,49 @@ func set_audio_stream(mp3: AudioStream) -> void:
 	audio.set_stream(mp3)
 
 ## Read a .txt file that was converted to an .osu file and parse it
-func read_osu_file(beatmap: String) -> void:
+func read_osu_file(beatmap: String) -> Dictionary:
 	var file = FileAccess.open(beatmap, FileAccess.READ)
 	var text = file.get_as_text()
 	
-	print(text)
+	var metadata : Dictionary = {
+		"General" : {},
+		"HitObjects": [],
+		"Metadata": {}
+	}
+	
+	var current_metadata : String = ""
+	
+	while not file.eof_reached():
+		var line = file.get_line()
+		print(line)
+		
+		if "[General]" in line:
+			current_metadata = line
+		if "[Metadata]" in line:
+			current_metadata = line
+		if "[HitObjects]" in line:
+			current_metadata = line
+		
+		
+		match current_metadata:
+			"[General]":
+				var key_value = line.split(':')
+				if len(key_value) == 2:
+					metadata["General"][key_value[0]] = key_value[1]
+			"[Metadata]":
+				var key_value = line.split(':')
+				if len(key_value) == 2:
+					metadata["Metadata"][key_value[0]] = key_value[1]
+			"[HitObjects]":
+				var raw_data = line.split(',')
+				if len(raw_data) >= 4:
+					var data = {
+							'x'     : raw_data[0],
+							'y'     : raw_data[1],
+							'time'  : raw_data[2],
+							'type'  : raw_data[3],
+						}
+					metadata["HitObjects"].append(data)
+	
+	return metadata
 	
