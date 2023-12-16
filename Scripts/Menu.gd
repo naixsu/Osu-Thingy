6e96cd17ff7d1bc2f747d3a029c653b5cfa3d598
naixsu <plnacua02@gmail.com>
2023-12-16 12:04:37 +0800

    added bg

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,16 +1,22 @@
 extends Control
 
-var songsDir : DirAccess
-var songsPath : String = "Songs/"
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 @onready var songsList = $SongsList
 
+var songsDirPath : String = "Songs/"
+var songsDir : DirAccess
+var songsListIndex : int = 0
+var selectedSongPath : String = ""
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	songsDir = DirAccess.open(songsPath)
+	songsDir = DirAccess.open(songsDirPath)
 	
 	init_song_list()
-	
+
 
 ## Initializes the SongsList list with songs under the 'Songs/' directory
 func init_song_list() -> void:
@@ -19,3 +25,15 @@ func init_song_list() -> void:
 	
 	for song in songs:
 		songsList.add_item(song)
+	
+	set_selected_song_path()
+
+
+## Helper function to set the song path to the selected song
+func set_selected_song_path():
+	selectedSongPath = songsList.get_item_text(songsListIndex)
+
+
+func _on_songs_list_item_selected(index):
+	songsListIndex = index
+	set_selected_song_path()
